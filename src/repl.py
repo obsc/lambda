89e6ec60b8faa655348aca6a5149a614bf330434
obsc <Abscuron@gmail.com>
2014-06-19 02:37:00 -0700

    Refactored print logic out of main repl loop for future plugin functionality

--- src/repl.py
+++ src/repl.py
@@ -35,11 +35,11 @@ def loadFile(filename):
 Evaluates a single expression on the global scope
 """
 def evalGlobal(s):
-    if s.strip() == '=':
+    s = removeComments(s).strip()
+    if s == '=':
         for k in GLOBALS:
             print '%s : %s' % (k, str(GLOBALS[k]))
         return
-    s = removeComments(s).strip()
     if '=' in s:
         pos = s.find('=')
         v = s[:pos].strip()
@@ -64,70 +64,108 @@ def evalGlobal(s):
 Evaluates a single expression
 """
 def evalOne(s):
+    if len(s) == 0:
+        return None
     try:
-        if len(s) > 0:
-            e = Expr.parse(s, copyGlobals())
-            incomplete = [e]
-            prev = None
-
-            while len(incomplete) > 0:
-                cur = incomplete.pop()
-                if cur.typ == 0:
-                    if cur.l == "print":
-                        cur.typ = 3
-                    elif cur.l in cur.env:
-                        cur.set(cur.env[cur.l].copy())
-                        incomplete.append(cur)
-                    elif cur.l != '()':
-                        print "Unbound Variable: %s" % cur.l
-                        return None
-                elif cur.typ == 2:
-                    if prev == cur.r:
-                        if cur.l.typ == 3 and cur.l.l == "print":
-                            if cur.r.typ == 3:
-                                print chr(cur.r.l)
-                            elif cur.r.typ == 1:
-                                num = cur.r.copy()
-                                cur.r.typ = 2
-                                
-                                cur.r.l = Expr('', num.env)
-                                cur.r.l.typ = 2
-                                cur.r.l.l = num
-                                cur.r.l.r = Expr('', num.env)
-                                cur.r.l.r.typ = 3
-                                cur.r.l.r.l = 'next'
-
-                                cur.r.r = Expr('', num.env)
-                                cur.r.r.typ = 3
-                                cur.r.r.v = 0
-                                print(cur)
-                                incomplete.append(cur)
-                                incomplete.append(cur.r)
-                            else:
-                                print "Unable to print"
-                                return None
-                        elif cur.l.typ == 3 and cur.l.l == 'next':
-                            cur.typ = 3
-                            cur.l = cur.r.l + 1
-                        elif cur.l.typ == 1:
-                            cur.l.r.subst(cur.l.l, cur.r)
-                            cur.set(cur.l.r.copy())
-                            incomplete.append(cur)
-                        else:
-                            print "Unable to Apply non-function"
-                            return None
+        expr = Expr.parse(s, copyGlobals())
+        exprs = [(expr, False)]
+        while len(exprs) > 0:
+            (e, flag) = exprs.pop()
+            if e.typ == 0:
+                if e.l in e.env:
+                    e.set(e.env[e.l].copy())
+                    exprs.append((e, False))
+                # elif plugins
+                elif e.l != '()':
+                    print "Unbound Variable: %s" % e.l
+                    return None
+            elif e.typ == 2:
+                if flag:
+                    if e.l.typ == 1:
+                        e.l.r.subst(e.l.l, e.r)
+                        e.set(e.l.r.copy())
+                        exprs.append((e, False))
+                    # elif plugins
                     else:
-                        incomplete.append(cur)
-                        incomplete.append(cur.r)
-                        incomplete.append(cur.l)
-                prev = cur
-
-            if VERBOSE:
-                print str(e)
-            return e
+                        print "Unable to Apply non-function"
+                        return None
+                else:
+                    exprs.extend([(e, True), (e.r, False), (e.l, False)])
+        if VERBOSE:
+            print str(expr)
+        return expr
     except (ParseError, FreeVariableException) as error:
         print error
     return None
+
+"""
+Evaluates a single expression
+"""
+# def evalOne(s):
+#     try:
+#         if len(s) > 0:
+#             e = Expr.parse(s, copyGlobals())
+#             incomplete = [e]
+#             prev = None
+
+#             while len(incomplete) > 0:
+#                 cur = incomplete.pop()
+#                 if cur.typ == 0:
+#                     if cur.l == "print":
+#                         cur.typ = 3
+#                     elif cur.l in cur.env:
+#                         cur.set(cur.env[cur.l].copy())
+#                         incomplete.append(cur)
+#                     elif cur.l != '()':
+#                         print "Unbound Variable: %s" % cur.l
+#                         return None
+#                 elif cur.typ == 2:
+#                     if prev == cur.r:
+#                         if cur.l.typ == 3 and cur.l.l == "print":
+#                             if cur.r.typ == 3:
+#                                 print chr(cur.r.l)
+#                             elif cur.r.typ == 1:
+#                                 num = cur.r.copy()
+#                                 cur.r.typ = 2
+                                
+#                                 cur.r.l = Expr('', num.env)
+#                                 cur.r.l.typ = 2
+#                                 cur.r.l.l = num
+#                                 cur.r.l.r = Expr('', num.env)
+#                                 cur.r.l.r.typ = 3
+#                                 cur.r.l.r.l = 'next'
+
+#                                 cur.r.r = Expr('', num.env)
+#                                 cur.r.r.typ = 3
+#                                 cur.r.r.v = 0
+#                                 print(cur)
+#                                 incomplete.append(cur)
+#                                 incomplete.append(cur.r)
+#                             else:
+#                                 print "Unable to print"
+#                                 return None
+#                         elif cur.l.typ == 3 and cur.l.l == 'next':
+#                             cur.typ = 3
+#                             cur.l = cur.r.l + 1
+#                         elif cur.l.typ == 1:
+#                             cur.l.r.subst(cur.l.l, cur.r)
+#                             cur.set(cur.l.r.copy())
+#                             incomplete.append(cur)
+#                         else:
+#                             print "Unable to Apply non-function"
+#                             return None
+#                     else:
+#                         incomplete.append(cur)
+#                         incomplete.append(cur.r)
+#                         incomplete.append(cur.l)
+#                 prev = cur
+
+#             if VERBOSE:
+#                 print str(e)
+#             return e
+#     except (ParseError, FreeVariableException) as error:
+#         print error
+#     return None
 
 """
 The repl
